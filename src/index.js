5ad02578c913ad194ff568300182af627c0b7c6d
ArielMant0 <buecher.apps@gmail.com>
2023-03-07 15:41:09 +0100

    Try providing complete defineStore

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import * as components from './components'
 
-import { vextNoteStore } from '@/store/note'
-import { vextStateStore } from '@/store/state'
-import { vextHistoryStore } from '@/store/history'
-import { vextAppStore } from '@/store/app'
+import { useVextNote } from '@/store/note'
+import { useVextState } from '@/store/state'
+import { useVextHistory } from '@/store/history'
+import { useVextApp } from '@/store/app'
 
 export function createVextPlugin() {
     const plugin = {
         install: function(app) {
-            app.provide("vextNoteStore", vextNoteStore);
-            app.provide("vextStateStore", vextStateStore);
-            app.provide("vextHistoryStore", vextHistoryStore);
-            app.provide("vextAppStore", vextAppStore);
+            app.provide("useVextNote", useVextNote);
+            app.provide("useVextState", useVextState);
+            app.provide("useVextHistory", useVextHistory);
+            app.provide("useVextApp", useVextApp);
             // auto import all components
             for (const componentKey in components) {
                 app.component(componentKey, components[componentKey])
