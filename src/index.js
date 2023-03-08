3ad68226bc5516690cd72f05639e83123f20a4b5
franziska.becker <franziska.becker@vis.uni-stuttgart.de>
2023-03-08 13:36:56 +0100

    trying a new way to export stores

--- src/index.js
+++ src/index.js
@@ -1,30 +1,21 @@
 import * as components from './components'
 
-import { useVextNote } from './store/note'
-import { useVextState } from './store/state'
-import { useVextHistory } from './store/history'
-import { useVextApp } from './store/app'
+import { useVextNote } from './store/note.js'
+import { useVextState } from './store/state.js'
+import { useVextHistory } from './store/history.js'
+import { useVextApp } from './store/app.js'
 
 function createVextPlugin() {
-    const plugin = {
-        install: function(app) {
-            // app.provide("useVextNote", useVextNote);
-            // app.provide("useVextState", useVextState);
-            // app.provide("useVextHistory", useVextHistory);
-            // app.provide("useVextApp", useVextApp);
-            // auto import all components
-            for (const componentKey in components) {
-                app.component(componentKey, components[componentKey])
-            }
-        }
+    // app.provide("useVextNote", useVextNote);
+    // app.provide("useVextState", useVextState);
+    // app.provide("useVextHistory", useVextHistory);
+    // app.provide("useVextApp", useVextApp);
+    // auto import all components
+    for (const componentKey in components) {
+        app.component(componentKey, components[componentKey])
     }
-    return plugin;
 }
 
-export {
-    createVextPlugin,
-    useVextNote,
-    useVextState,
-    useVextHistory,
-    useVextApp
-}
+export default createVextPlugin
+
+export { useVextNote, useVextState, useVextHistory, useVextApp }
